4a550c416ddd12b8c34e94843121dad1b1656054
minhoshin <minho.shin@gmail.com>
2016-10-04 18:55:56 +0900

    layout

---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(<CommentBox/>,document.getElementById('content'));
