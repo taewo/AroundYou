32675a7b47072f751baac83c7bc882da8340d05b
minhoshin <minho.shin@gmail.com>
2016-10-09 00:31:12 +0900

    server file update

--- build/main.js
+++ build/main.js
@@ -4,23 +4,27 @@ var _express = require('express');
 
 var _express2 = _interopRequireDefault(_express);
 
-var _posts = require('./routes/posts');
-
-var _posts2 = _interopRequireDefault(_posts);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express2.default)();
-
 var port = 3000;
 
 app.use('/', _express2.default.static(__dirname + './../client/public'));
 
-app.get('/hello', function (req, res) {
-  return res.send("Can you hear me?");
+app.get('/', function (req, res) {
+  res.render('./../client/public/index.html');
 });
 
-app.use('/posts', _posts2.default);
+app.get('/test', function (req, res) {
+  res.send('test');
+});
+
+// app.get('/hello', (req, res) => {
+//   return res.send("Can you hear me?");
+// });
+//
+// import posts from './routes/posts';
+// app.use('/posts', posts);
 
 var server = app.listen(port, function () {
   console.log("Express listening on port", port);
--- server/main.js
+++ server/main.js
@@ -1,17 +1,23 @@
 import express from 'express';
-
 const app = express();
-
 let port = 3000;
 
 app.use('/', express.static(__dirname + './../client/public'));
 
-app.get('/hello', (req, res) => {
-  return res.send("Can you hear me?");
+app.get('/', function(req, res){
+  res.render('./../client/public/index.html');
 });
 
-import posts from './routes/posts';
-app.use('/posts', posts);
+app.get('/test', function(req, res){
+  res.send('test');
+});
+
+// app.get('/hello', (req, res) => {
+//   return res.send("Can you hear me?");
+// });
+//
+// import posts from './routes/posts';
+// app.use('/posts', posts);
 
 const server = app.listen(port, () => {
   console.log("Express listening on port", port);
